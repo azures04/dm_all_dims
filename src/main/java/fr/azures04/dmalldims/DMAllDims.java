1af02616641b195c3015617a86b36c7fb4ebe565
azures04 <gilleslazure04@gmail.com>
2025-06-02 04:27:08 +0200

    some fixs

--- src/main/java/fr/azures04/dmalldims/DMAllDims.java
+++ src/main/java/fr/azures04/dmalldims/DMAllDims.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 
-@Mod(modid = Constants.mod_id, version = Constants.version, name = Constants.mod_name, serverSideOnly = true)
+@Mod(modid = Constants.mod_id, version = Constants.version, name = Constants.mod_name, serverSideOnly = true, acceptableRemoteVersions = "*")
 public class DMAllDims {
 	
 	@EventHandler
